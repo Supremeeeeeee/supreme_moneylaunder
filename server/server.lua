40402ba02a2ab82090386c7e0fdb975c4e41b594
DuncanEll <59232670+Supremeeeeeee@users.noreply.github.com>
2021-08-25 23:57:09 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,13 +3,12 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('supreme_moneylaunder:GetCash')
-AddEventHandler('supreme_moneylaunder:GetCash', function(itemName, price)
+AddEventHandler('supreme_moneylaunder:GetCash', function()
 	local xPlayer  = ESX.GetPlayerFromId(source)
-        local randomLaunder = math.random(1000,2000)
 		if xPlayer.getAccount('black_money').money > 0 then
-			xPlayer.removeAccountMoney('black_money', randomLaunder)
-			xPlayer.addMoney(randomLaunder)
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You laundered $' .. randomLaunder, })
+			xPlayer.removeAccountMoney('black_money', 1000)
+			xPlayer.addMoney(1000)
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You laundered $1000', })
 		else
 			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You dont have any ', })
 		end
