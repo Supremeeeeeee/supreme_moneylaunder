ee3b3ebe7b4a273093475048849d1b0696dd2b08
DuncanEll <59232670+Supremeeeeeee@users.noreply.github.com>
2021-08-26 09:20:45 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -5,7 +5,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterServerEvent('supreme_moneylaunder:GetCash')
 AddEventHandler('supreme_moneylaunder:GetCash', function()
 	local xPlayer  = ESX.GetPlayerFromId(source)
-		if xPlayer.getAccount('black_money').money > 0 then
+		if xPlayer.getAccount('black_money').money > 999 then
 			xPlayer.removeAccountMoney('black_money', 1000)
 			xPlayer.addMoney(1000)
 			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You laundered $1000', })
